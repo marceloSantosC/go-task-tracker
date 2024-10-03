9071787b2833cfe906ae92176d658af12e617291
Marcelo <marcelo17082000@gmail.com>
2024-10-02 22:52:34 -0300

    Created basic app init in main

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"go-task-tracker/repository"
+	"go-task-tracker/service"
+	"log"
+	"log/slog"
+	"os"
+	"time"
 )
 
 func main() {
 
-	filename := "task_list"
-	tr, _ := repository.NewTaskRepositoryFile(filename)
-	_ = tr
+	filename := "task_list.json"
+	repo, err := repository.NewTaskRepositoryFile(filename)
+	if err != nil {
+		log.Panicf("failed to start app: %s", err)
+	}
+
+	file, err := os.Create(fmt.Sprintf("logs_%d", time.Now().UnixMilli()))
+	if err != nil {
+		log.Panicf("failed to start app: %s", err)
+	}
+
+	l := slog.New(slog.NewTextHandler(file, nil))
+
+	l.Info("Initialized app using file.", slog.String("file", filename))
+	service.NewTaskService(&repo, l)
+
+	fmt.Println("# GO Task Tracker #")
 }
